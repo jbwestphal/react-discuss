4c1e1c53d96656a1c9a35f83236b3bdca3ae1293
Jessica Westphal <pretty.jehh.s2@hotmail.com>
2018-03-06 18:09:08 -0300

    App and server organization. Correction in Express ports

--- client/src/components/test/Modal.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {shallow} from 'enzyme'
-import Modal from '../Modal'
-
-describe('<Modal />', () => {
-  it('shallow without crashing', () => {
-    expect(shallow(<Modal />))
-  })
-})
--- client/src/components/test/PostNew.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import { PostNew } from '../PostNew'
-
-describe('<PostNew />', () => {
-  it('shallow renders correctly', () => {
-    expect(shallow(<PostNew onCreatePost={() => {}} createPost={() => {}} />))
-  })
-})
