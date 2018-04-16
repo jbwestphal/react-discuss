cbf4df0928b4052a3825f99ed5fb7a99806ed345
Jessica Westphal <jbwestphal85@gmail.com>
2018-04-15 21:30:19 -0300

    feat: Add test of integration for Post and PostList components

--- public/client/src/components/test/Post.test.js
+++ public/client/src/components/test/Post.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
 import { Post } from '../Post'
 
 describe('<Post />', () => {
@@ -11,11 +12,39 @@ describe('<Post />', () => {
     commentCount: 2,
     deleted: false,
     id: 'KMH86HLA',
-    title: 'Post titlte',
+    title: 'Post title',
     voteScore: 2
   }
 
+  // self render
   it('shallow without crashing', () => {
     expect(shallow(<Post onClickVote={() => {}} onDeletePost={() => {}} width="" post={item} />))
+  })
+
+  // children testing render
+  it('mounts correctly', () => {
+    expect(mount(
+    <MemoryRouter>
+      <Post onClickVote={() => {}} onDeletePost={() => {}} width="" post={item} />
+    </MemoryRouter>
+    ))
+  })
+
+  it('renders detailButton if btnDetail is equals to true', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <Post btnDetail={true} onClickVote={() => {}} onDeletePost={() => {}} width="" post={item} />
+      </MemoryRouter>);
+
+    expect(wrapper.find('a.detailButton').length).toBe(1)
+  })
+
+  it('DONT renders detailButton if btnDetail is equals to false', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <Post btnDetail={false} onClickVote={() => {}} onDeletePost={() => {}} width="" post={item} />
+      </MemoryRouter>);
+
+    expect(wrapper.find('a.detailButton').length).toBe(0)
   })
 })
--- public/client/src/components/test/PostList.test.js
+++ public/client/src/components/test/PostList.test.js
@@ -1,20 +1,31 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
 import PostList from '../PostList'
 
 describe('<PostList />', () => {
+
+  const posts = [{
+    author: 'jess',
+    body: 'testing texts',
+    category: 'react',
+    commentCount: 2,
+    deleted: false,
+    id: 'KMH86HLA',
+    title: 'Post titlte',
+    voteScore: 2
+  }]
+
   it('shallow renders correctly', () => {
-    expect(shallow(<PostList posts={
-      [{
-        author: 'jess',
-        body: 'testing texts',
-        category: 'react',
-        commentCount: 2,
-        deleted: false,
-        id: 'KMH86HLA',
-        title: 'Post titlte',
-        voteScore: 2
-      }]
-    } voteOnPost={() => {}} deletePost={() => {}} sortPost={() => {}} />))
+    expect(shallow(<PostList posts={posts} voteOnPost={() => {}} deletePost={() => {}} sortPost={() => {}} />))
+  })
+
+  it('expects to map trough an array of items and creates a Post for each one of them', () => {
+    const wrapper = mount(
+    <MemoryRouter>
+      <PostList posts={posts} voteOnPost={() => {}} deletePost={() => {}} sortPost={() => {}} />
+    </MemoryRouter>)
+
+    expect(wrapper.find('div.post-container').length).toBe(1)
   })
 })
